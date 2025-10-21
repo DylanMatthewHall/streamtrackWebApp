84cf7f9b8cea14d0db4091def57ac661991eab50
DylanHallLaptop <DylanMatthewHall@outlook.com>
2025-10-21 11:23:32 -0500

    First Commit

---
+++ src/main/java/com/dylan/streamtrack/StreamtrackApplication.java
@@ -0,0 +1,13 @@
+package com.dylan.streamtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dylan/streamtrack/StreamtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.streamtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
